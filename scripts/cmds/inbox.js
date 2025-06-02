788ac7d7d784ca07fc354a62a9ab2e2d9cbb349a
Mah MUD <mahmudx077@gmail.com>
2025-06-02 12:11:52 +0600

    Create inbox.js

---
+++ scripts/cmds/inbox.js
@@ -0,0 +1,25 @@
+module.exports = {
+  config: {
+    name: "inbox",
+    aliases: ["in"],
+    version: "1.7",
+    author: "MahMUD",
+    countDown: 5,
+    role: 0,
+    category: "system"
+  },
+  onStart: async function({ api, event, args, message }) {
+    try {
+      const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68); 
+      if (this.config.author !== obfuscatedAuthor) {
+      return api.sendMessage("You are not authorized to change the author name.\n", event.threadID, event.messageID);
+    }
+
+      const query = encodeURIComponent(args.join(' '));
+      message.reply("𝐛𝐚𝐛𝐲 𝐜𝐡𝐞𝐜𝐤 𝐲𝐨𝐮𝐫 𝐢𝐧𝐛𝐨𝐱 🐤", event.threadID);
+      api.sendMessage("𝐡𝐢 𝐛𝐚𝐛𝐲😘", event.senderID);
+    } catch (error) {
+      console.error("error baby: " + error);
+    }
+  }
+};
